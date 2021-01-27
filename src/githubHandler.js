42d173b21a440378bc658572171bc848e151499d
Ichii <45918948+IchiiDev@users.noreply.github.com>
2021-01-27 16:57:00 +0100

    Added requestReview function

--- src/githubHandler.js
+++ src/githubHandler.js
@@ -16,5 +16,22 @@ module.exports = {
                     resolve(json);
                 });
         });
+    },
+
+    requestReview(pull, repo, user) {
+        return new Promise((resolve, rejects) => {
+            const data = {
+                method: "POST",
+                headers: { accept: 'application/vnd.github.v3+json', authorization: `${GH_USERNAME}:${GH_PAT}` },
+                body: JSON.stringify({ reviewers: [ user ] })
+            };
+            fetch(`https://api.github.com/repos/${repo}/pulls/${pull}/requested_reviewers`, data)
+                .then(response => response.json())
+                .then(json => {
+                    console.log(json);
+                    resolve(json);
+                });
+        });
     }
+
 }
--- src/prListener.js
+++ src/prListener.js
@@ -1,5 +1,5 @@
 const { router, config } = require("../index");
-const { assignUser } = require("./githubHandler");
+const { requestReview } = require("./githubHandler");
 
 router.post("/webhook/pull_request", async (req, res) => {
     if (isPing(req.body)) {
@@ -16,7 +16,7 @@ router.post("/webhook/pull_request", async (req, res) => {
         }
 
         let isOK = true;
-        await assignUser(req.body.number, req.body.repository.full_name, assignee)
+        await requestReview(req.body.number, req.body.repository.full_name, assignee)
             .then(() => {})
             .catch(error => { isOK = false; res.status(500).json({ message: "500: Internal Server Error", error: error }); });
         if (isOK == true) res.status(200).json({ message: "200: All clear" });
